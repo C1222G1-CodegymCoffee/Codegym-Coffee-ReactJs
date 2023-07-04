34d2c4c029f7758091572c918c61225b29352eab
khainlv99 <117066707+khainlv99@users.noreply.github.com>
2023-07-04 12:44:19 +0700

    KhaiNLV update list table, findbyid table,list order,reset table

--- codegym_coffee/src/App.js
+++ codegym_coffee/src/App.js
@@ -5,6 +5,10 @@ import {Routes, Route} from 'react-router-dom'
 import {HomePage} from "./component/Homepage/HomePage";
 import {Sale} from "./component/Sale/Sale";
 import {ListFeedback} from "./component/feedback/ListFeedback";
+import {EmployeeList} from "./component/employee/EmployeeList";
+import {AddEmployee} from "./component/employee/AddEmployee";
+import List from "./component/bill/List";
+
 
 
 function App() {
@@ -14,6 +18,9 @@ function App() {
                 <Route path="/" element={<HomePage/>}></Route>
                 <Route path="/sale" element={<Sale/>}></Route>
                 <Route path="/feedback" element={<ListFeedback/>}></Route>
+                <Route path="/employee" element={<EmployeeList/>}></Route>
+                <Route path="/employee_create" element={<AddEmployee/>}></Route>
+                <Route path="/bill" element={<List/>}></Route>
             </Routes>
       </>
   );
