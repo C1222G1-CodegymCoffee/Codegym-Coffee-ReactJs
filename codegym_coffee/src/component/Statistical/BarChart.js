fe9a00c8d3ced035a4cd595519967922d95b4018
dinhminhthong <dinhminhthong14022001@gmail.com>
2023-07-05 17:59:21 +0700

    update source code

--- codegym_coffee/src/component/Statistical/BarChart.js
+++ codegym_coffee/src/component/Statistical/BarChart.js
@@ -1,13 +1,17 @@
 import React, {useState} from 'react';
 import {ResponsiveContainer, BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar} from 'recharts';
 import {findAllSum} from "../../service/statistical/StatisticalService"
-import {Formik,Form,Field,ErrorMessage} from "formik";
+import '../../css/News/Statistical.css'
+import {Formik, Form, Field} from "formik";
 
 function ChartExample() {
-    const [data1, setData1] = useState([])
+    const [data1, setData1] = useState(null)
 
     return (
         <>
+            {/*<div id='abc'>*/}
+            <div id='abc'>
+                <div className="form-container">
             <h1 className="custom-header">Quản lí thu nhập</h1>
 
             <Formik initialValues={{
@@ -15,8 +19,14 @@ function ChartExample() {
                 dateBefore:""
 
             }} onSubmit={async (values) => {
-                let res = await findAllSum(values.dateAfter, values.dateBefore)
-                setData1(res.data)
+                try {
+                    console.log(values)
+                    let res = await findAllSum(values.dateAfter, values.dateBefore)
+                    setData1(res.data)
+                } catch (e) {
+                    console.log(e)
+                }
+
             }}>
                 <Form>
                     <div className="form-row">
@@ -32,26 +42,30 @@ function ChartExample() {
                     </div>
                 </Form>
             </Formik>
+                <ResponsiveContainer className="chart"
+                                     height={300}
+                                     >
+                    <BarChart
+                        width={600}
+                        height={300}
+                        data={data1}
+                        margin={{top: 5, right: 30, left: 20, bottom: 5}}
+                    >
+                        <XAxis dataKey="name" />
+                        <YAxis/>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <Tooltip/>
+                        <Legend/>
+                        <Bar dataKey="value" fill="#8884d8"/>
+                    </BarChart>
+                </ResponsiveContainer>
+                </div>
 
-
-            <ResponsiveContainer className="chart" height={300}>
-                <BarChart
-                    width={600}
-                    height={300}
-                    data={data1}
-                    margin={{top: 5, right: 30, left: 20, bottom: 5}}
-                >
-                    <XAxis dataKey="name"/>
-                    <YAxis/>
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <Tooltip/>
-                    <Legend/>
-                    <Bar dataKey="value" fill="#8884d8"/>
-                </BarChart>
-            </ResponsiveContainer>
+            </div>
+            {/*</div>*/}
         </>
 
-);
+    );
 }
 
 export default ChartExample;
