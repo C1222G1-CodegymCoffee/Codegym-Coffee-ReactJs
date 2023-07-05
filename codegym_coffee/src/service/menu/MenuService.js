6fabe5dd2e1a0342d2f851e5b4dfea7bdbb8945a
NguyenDanhh <danhnguyen15112003@gmai.com>
2023-07-05 17:55:31 +0700

    update source code react-menu

--- codegym_coffee/src/service/menu/MenuService.js
+++ codegym_coffee/src/service/menu/MenuService.js
@@ -1,19 +1,23 @@
 import axios from "axios";
 
 export const getAllProduct = async () => {
-    const res = await axios.get("http://localhost:8080/menu")
+    const res = await axios.get("http://localhost:8080/v2/menu")
     return res.data;
 }
 
 export const getAllTypeProduct = async () => {
-    const res = await axios.get("http://localhost:8080/menu/type-product")
+    const res = await axios.get("http://localhost:8080/v2/menu/type-product")
     return res.data;
 }
 
 export const getAllProductByType = async (type) => {
-    const res = await axios.get(`http://localhost:8080/menu/product-by-type?type=${type}`)
+    const res = await axios.get(`http://localhost:8080/v2/menu/product-by-type?type=${type}`)
+    return res.data;
+}
+export const getProductByName = async (nameProduct) => {
+    const res = await axios.get(`http://localhost:8080/v2/menu/product-by-name?nameProduct=${nameProduct}`)
     return res.data;
 }
 export const addToBill = async (products) => {
-    await axios.post("http://localhost:8080/menu/add-to-bill",products)
+    await axios.post("http://localhost:8080/v2/menu/add-to-bill",products)
 }
